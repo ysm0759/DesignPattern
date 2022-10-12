5e35318ed595ade846d51cd66574d6160bf42600
YuSungMin <ysm0759@naver.com>
2022-10-12 17:52:40 +0900

    초기 커밋.

---
+++ Design Pattern/Iterator/Array.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Pattern.Iterator
+{
+    class Array : Aggregator
+    {
+        private Item[] items;
+        public Array(Item[] items)
+        {
+            this.items = items;
+        }
+
+        public Item GetItem(int index)
+        {
+            return items[index];
+        }
+
+        public int GetCount()
+        {
+            return items.Length;
+        }
+
+
+        public Iterator Iterator()
+        {
+            return new ArrayIterator(this);
+        }
+
+    }
+}
---
+++ Design Pattern/Iterator/IteratorMain.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Pattern.Iterator
+{
+    class IteratorMain
+    {
+        static void Main(string[] args)
+        {
+            Item[] items =
+            {
+                new Item("CPU",1000),
+                new Item("Keyboard", 2000),
+                new Item("Mouse", 3000),
+                new Item("HDD", 50),
+            };
+
+            Array array = new Array(items);
+            Iterator iter = array.Iterator();
+            Console.WriteLine(array.GetItem(1));
+            while(iter.Next())
+            {
+                Item item = (Item)iter.Current();
+                Console.WriteLine(item.ToString());
+            }
+
+        }
+    }
+}
