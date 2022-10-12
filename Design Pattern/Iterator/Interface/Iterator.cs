45a8cd14ea7d38f5485c561a9125129fff66a3be
YuSungMin <ysm0759@naver.com>
2022-10-12 17:52:40 +0900

    Iterator

---
+++ Design Pattern/Iterator/Interface/Iterator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Design_Pattern.Iterator
+{
+
+    public interface Iterator
+    {
+        Boolean Next();
+        Object Current();
+    }
+}
+
+
+
+
+
---
+++ Design Pattern/Iterator/Item.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Design_Pattern.Iterator
+{
+
+
+    class Item
+    {
+        private String name;
+        private int cost;
+
+
+        public Item(String name, int cost)
+        {
+            this.name = name;
+            this.cost = cost;
+        }
+
+        public override string ToString()
+        {
+            return "(" + name + ", " + cost + " )";
+        }
+    }
+}
